a80b623dc38ffe7d206d77947f23c7f7016476e2
awake <lqh19980716@163.com>
2024-08-06 20:27:28 +0800

    岂不闻天无绝人之路

---
+++ c#/code/day01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Classes;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var account = new BankAccount("lqhao", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+
+Console.WriteLine(account.GetAccountHistory());
