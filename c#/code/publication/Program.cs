6282ed422d193589b7bc0d1ed851d5103d8341cd
awake <lqh19980716@163.com>
2024-09-11 14:25:40 +0800

    岂不闻天无绝人之路

---
+++ c#/code/publication/Program.cs
@@ -0,0 +1,70 @@
+﻿public class ClassExample
+{
+    public static void Main()
+    {
+        var book = new Book("The Tempest", "0971655819", "Shakespeare, William",
+                            "Public Domain Press");
+        ShowPublicationInfo(book);
+        book.Publish(new DateTime(2016, 8, 18));
+        ShowPublicationInfo(book);
+
+        var book2 = new Book("The Tempest", "Classic Works Press", "Shakespeare, William");
+        Console.Write($"{book.Title} and {book2.Title} are the same publication: " +
+              $"{((Publication)book).Equals(book2)}");
+    }
+
+    public static void ShowPublicationInfo(Publication pub)
+    {
+        string pubDate = pub.GetPublicationDate();
+        Console.WriteLine($"{pub.Title}, " +
+                  $"{(pubDate == "NYP" ? "Not Yet Published" : "published on " + pubDate):d} by {pub.Publisher}");
+    }
+}
+// The example displays the following output:
+//        The Tempest, Not Yet Published by Public Domain Press
+//        The Tempest, published on 8/18/2016 by Public Domain Press
+//        The Tempest and The Tempest are the same publication: False
+
+
+
+namespace NullableIntroduction
+{
+    public enum QuestionType
+    {
+        YesNo,
+        Number,
+        Text
+    }
+    [ Test ]
+    public class SurveyQuestion
+    {
+        public string QuestionText { get; }
+        public QuestionType TypeOfQuestion { get; }
+        
+        public SurveyQuestion(QuestionType typeOfQuestion, string text) =>
+        (TypeOfQuestion, QuestionText) = (typeOfQuestion, text);
+    }
+
+    //声明注解
+    //AttributeUsage 声明范围
+    //AllowMultiple 是否可以多次声明 对同一个位置
+    //Inherited 是否对子类 也加上该注解
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.ReturnValue | AttributeTargets.Assembly,
+    AllowMultiple=true,Inherited=false)]
+    class Test:Attribute
+    {
+
+        public Test(Flag flag = Flag.None)
+        {  
+
+        }
+
+    }
+
+       public enum Flag
+    {
+        None,
+        Input,
+        Output
+    }
+}
