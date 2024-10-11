c9e1e8f90961c7e515c22336e0f13c913ad5b09b
awake <lqh19980716@163.com>
2024-10-11 15:29:40 +0800

    c#整理

--- c#/code/attr/obj/Debug/net8.0/attr.AssemblyInfo.cs
+++ c#/code/attr/obj/Debug/net8.0/attr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("attr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abe62615df71c47e0a75a7fd9c5e942e7c4b1aa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6282ed422d193589b7bc0d1ed851d5103d8341cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("attr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("attr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
